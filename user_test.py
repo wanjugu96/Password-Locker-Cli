0bfd1b3ab2ac9f987c87aed58438261493e8a3ec
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-09-04 11:27:46 +0300

    defining user properties

---
+++ user_test.py
@@ -0,0 +1,11 @@
+#create a password locker 
+#with my details, a login username and password
+
+class User:
+    """Class generates a password locker accounts
+    """
+    def __init__(self,username,password):
+        self.username=username
+        self.password=password
+
+   
