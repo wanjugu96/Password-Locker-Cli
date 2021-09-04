1cfb49f1dd938932ff2ebead17e02f38c247a8ad
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-09-04 11:32:50 +0300

    add user class properties to the right user class file

---
+++ user.py
@@ -0,0 +1,17 @@
+#create a password locker 
+#with my details, a login username and password
+
+class User:
+    User_list=[]
+    """
+    Class generates a password locker accounts
+    """
+    def __init__(self,username,password):
+        self.username=username
+        self.password=password
+
+    def save_user(self):
+        User.
+
+
+   
