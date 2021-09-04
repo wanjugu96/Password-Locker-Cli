a728b53e2a6adc45488b5899ab6b87580a6102fc
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-09-04 12:23:46 +0300

    adding test to test the initiaization of object

--- user_test.py
+++ user_test.py
@@ -1,6 +1,5 @@
 import unittest
 from user import User
-
 class TestUser(unittest.TestCase):
     """
     Test class that defines test cases for our contact class behaviours
@@ -11,6 +10,15 @@ class TestUser(unittest.TestCase):
         """
         set up method to run before each testcase
         """
-        self.new_User=User("wanjugu","Test@123$")
+        self.new_user=User("wanjugu","Test@123$",{"twitter":"1w2@3","instagram":"wena"})
 
-        def test_init(self):
+    def test_init(self):
+        """
+        Test to test if user object is being intialized properly
+        """
+        self.assertEqual(self.new_user.username,"wanjugu")
+        self.assertEqual(self.new_user.password,"Test@123$")
+        self.assertEqual(self.new_user.Account_credentials,{"twitter":"1w2@3","instagram":"wena"})
+
+if __name__=='__main__':
+    unittest.main()
