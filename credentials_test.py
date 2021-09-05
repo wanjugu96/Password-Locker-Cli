124117f5525f3c2bfd4dd0b41950551d445f6489
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-09-05 09:04:40 +0300

    adding tests for credentials intialization and saving.

--- credentials_test.py
+++ credentials_test.py
@@ -1,8 +1,24 @@
 from credentials import Credentials
 import unittest
 
-Class TestCredentials(unittest.TestCase):
+class TestCredentials(unittest.TestCase):
+    """
+    Test calss that defines test cases for our credentials class behaviours
+    unnittest.TetsCase:Testcase class that helps crete testcases
+    """
     def setUp(self):
+        """
+        Setup method to run before each testcase
+        """
+        self.new_credential=Credentials("Facebook","123456")
+
+    def test_init(self):
+        """
+        Test to confirm the proper initialization of a credential
+        """
+
+        self.assertEqual(self.new_credential.acc_name,"Facebook")
+        self.assertEqual(self.new_credential.acc_password,"123456")
 
 
 
