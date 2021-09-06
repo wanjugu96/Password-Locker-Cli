1de268b4036ed17813e195d7d31f01c83ec53337
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-09-06 11:18:36 +0300

    Adding interface UI changes

--- Interface.py
+++ Interface.py
@@ -38,133 +38,192 @@ def delete_credential(credential):
 
     
 def main():
-    save_credential(create_user("wanjugu","1234"))
-    save_credential(create_user("Doe","doe@123"))
+    save_users(create_user("wanjugu","1234",[]))
+    save_users(create_user("Doe","doe@123",[]))
 
-    #while True:
-    print("****************************************************************************************************************************")
-
-    print('\n')
-    print("Welcome to passwordLocker ")
+    
+    print("*************************************************************************************************************************************************************")
     print('\n')
 
-    print("****************************************************************************************************************************")
+    print("                                                         HELLO !                                                           ")
     print('\n')
-    print("please Enter :")
-    print("signin - if already have an acccount") 
-    print("create - create an account")
+    print("                                                  WELCOME TO PASSWORDLOCKER                       ")
+    print('\n')
 
-    login_input=input().lower()
+    print("*************************************************************************************************************************************************************")
+    print('\n')
+    # print("please Enter :")
+    # print("signin - if already have an acccount") 
+    # print("create - create an account")
 
-    if login_input=='create':  
-        print("Create your account below")
-        print("Enter your username")
-        acc_username=input()
-        print("Enter your password")
-        acc_password=input()
+    # login_input=input().lower()
 
-        allcreds=[]
-        user=create_user(acc_username,acc_password,allcreds)
-        save_users(user)
-        print("Account created Successfully")
+    # if login_input=='create':  
+    print(" Create your account below")
+    print('\n')
+    print(" Enter your username")
+    acc_username=input()
+    print('\n')
+    print(" Enter your password")
+    acc_password=input()
 
-        print("login to your account yes or no ?")
-        loginyesno=input()
+    allcreds=[]
+    user=create_user(acc_username,acc_password,allcreds)
+    save_users(user)
+    print("*******************************************************************************************************************************")
 
-
-    elif login_input=="login":
-        print("Enter your username:")
-        username=input()
-        for user in display_users:
-            if user.acc_username ==user.acc_password:
-                print("succsessfully logged in")
-
-   
-    print(f"The username is {user.username} password is {user.password}")
-   # if user.password==login_password:
-
+    print(" Account created Successfully ")
+    print('\n')
+    while True:
+        print(" login to your account below:")
         print('\n')
-        while True:
-            print("What would you like to do:")
+        print("*************************************************************************************************************************************************************")
+
+        print(' Enter username:')
+        login_name=input()
+        print('\n')
+        print(' Enter password:')
+        print('\n')
+        login_password=input()
+        print('\n')
+
+        print("*************************************************************************************************************************************************************")
+        if user.password==login_password:
+            print(" succsessfully logged in")
             print('\n')
-            print("Enter:store-To Store existinng account password , view-To view saved passwords , new-To delete saved credentials,delete-to delete a saved credential  ")
-            option=input().lower()
+            print("*************************************************************************************************************************************************************")
+        
+            while True:
+                print("*************************************************************************************************************************************************************")
 
 
-            if option=='store':
-                print('Enter accoutn name you want store eg twitter')
-                account_name=input()
+                print("  What would you like to do? ...Use the codes below:")
                 print('\n')
-                print('Enter username of the account you want store eg twitter')
-                account_username=input()
+                print("  store - To Store existinng account passwords ")
+                print("  view -  To view saved account credentials") 
+                print("  new -   To save new accounts credentials")
+                print("  delete -To delete a saved credential")
+                print('\n')
+                print("*************************************************************************************************************************************************************")
 
-                print('Enter password for that account eg twittes123')
-                account_password=input()
-               
-               
-                account_name=create_credential(account_name,account_username,account_password)
-                save_credential(account_name)
+                option=input().lower()
+                print('\n')
 
 
-            elif option=='view':
-                if display_credentials():
-                    print("your saved credentials are:")
+                if option=='store':
+                    print('Enter account name you want store eg twitter')
+                    account_name=input()
+                    print('\n')
+                    print(f'Enter username of {account_name} account')
+                    account_username=input()
+                    print('\n')
+                    print(f'Enter password for {account_name}')
+                    account_password=input()
+                    print('\n')
+                    print(f"Credential for {account_name} saved successfully")
+                    print('\n')
 
-                    user.credential_list=display_credentials()
-                    for credential in user.credential_list:
-                        print(f"{credential.acc_name} credentials are: Username is {credential.acc_username} password is:{credential.acc_password}")
-                        #print(f")
-                else:
-                    print("You dont have saved credentials")
+                
+                    account_name=create_credential(account_name,account_username,account_password)
+                    save_credential(account_name)
 
+
+                elif option=='view':
+                    if display_credentials():
+                        print('\n')
+                        print("your saved credentials are:")
+                        print('\n')
+
+
+                        user.credential_list=display_credentials()
+                        for credential in user.credential_list:
+
+                            print(f" -  {credential.acc_name} credentials are: Username is {credential.acc_username} password is:{credential.acc_password}")
+                            
+                            print("__________________________________________________________________________________________________________________________________________________________________")
+
+
+                            print("______-----------__________________________________________________________________________________________________________________________________________________")
+                    else:
+                        print('\n')
+
+                        print("You dont have saved credentials")
+
+                        
+
+                elif option=='new':
                     
+                    
+                    print("Enter the name of the new account youd like to save:")
+                    new_acc_name=input()
+                    print('\n')
+                    print(f"Enter the User name of {new_acc_name} account")
+                    new_acc_username=input()
+                    print('\n')
+                    while True:
 
-            elif option=='new':
-                print("Enter the name of the new account youd like to save:")
-                new_acc_name=input()
-                print("Enter the User name of the new account youd like to save:")
-                new_acc_username=input()
+                        print(f"Would you like a generated password for {new_acc_name} account yes or no ?")
+                        yes_no=input().lower()
+                        print('\n')
 
-                print("Would you like a generated password yes or no ?")
-                yes_no=input().lower()
+                        if yes_no=='yes':
+                            new_acc_pass=generate_password()
+                            print(f"your generated password is{new_acc_pass}")
+                            print('\n')
+                            print(f"Credential for {new_acc_name} saved successfully")
+                            save_credential(create_credential(new_acc_name,new_acc_username,new_acc_pass))
+                            break
+                        elif yes_no=='no':
+                            print('please enter the password to new account:')
+                            new_acc_pass=input()
+                            print('\n')
+                            print("Credential for {new_acc_name} saved successfully")
+                            save_credential(create_credential(new_acc_name,new_acc_username,new_acc_pass))
+                            break
+                        
+                        else:
+                            #print('\n')
+                            print("please enter valid option yes or no")
+                            print('\n')
+                            continue
+                            
 
-                if yes_no=='yes':
-                    new_acc_pass=generate_password()
-                    print(f"your generated password is{new_acc_pass}")
-                elif yes_no=='no':
-                    print('please enter the password to new account:')
-                    new_acc_pass=input()
+
+                        
+                        
+
+                    # else:
+                    #     print("please enter valid option yes or no")
+                    
+                    
                 
-                save_credential(create_credential(new_acc_name,new_acc_username,new_acc_pass))
 
-                # else:
-                #     print("please enter valid option yes or no")
-                
-                
-               
-
-            elif option=='delete':
-                print("what is the name of the credential youd like to delete?")
-                delete_name=input()
-                for credential in user.credential_list:
-                    if credential.acc_name!=delete_name:
-                        (f"Credentials for {delete_name} do not exist")
-                       
-            
-                    elif credential.acc_name==delete_name:
-                        delete_credential(credential)
-                        print(f"successfully deleted {credential.acc_name} credentials")
+                elif option=='delete':
+                    print("what is the name of the credential youd like to delete?")
+                    delete_name=input()
+                    print('\n')
+                    for credential in user.credential_list:
+                        if credential.acc_name!=delete_name:
+                            (f"Credentials for {delete_name} do not exist")
                         
                 
+                        elif credential.acc_name==delete_name:
+                            delete_credential(credential)
+                            print(f"successfully deleted {credential.acc_name} account credentials")
+                            print('\n')
+                            
+                    
 
 
-            else:
-                print("please enter a valid option.")
-            
+                else:
+                    print("Oppsie please enter a valid option.")
+                
 
+        else:
+            print("Wrong password please try again!")
 
-    else:
-        print("Wrong password")
+            continue
+
 
         # for user in display_users():
         # print(f"user added name is {user.username}")
